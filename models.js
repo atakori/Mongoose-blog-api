af04b825d33dbb1481717f00050346a69f30dd87
Alfredo <atakori@outlook.com>
2017-10-21 22:06:48 -0400

    added schema

---
+++ models.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const blogSchema = mongoose.Schema({
+	title: String,
+	content: String,
+	author: {
+		firstName: String, 
+		lastName: String
+	},
+	created: Date
+});
+
+blogSchema.virtual('fullNameString').get(function() {
+	return `${this.author.firstName}  ${this.author.lastName}`.trim()
+});
+
+blogSchema.methods.apiRepr = function() {
+
+	return {
+		id: this._id
+		title: this.title,
+		content: this.content,
+		author: this.fullNameString,
+		created: this.created
+	}
+}
+
+const blogPost = mongoose.model('blogPost', blogSchema);
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
